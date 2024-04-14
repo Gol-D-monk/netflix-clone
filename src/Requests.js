3a5a4c33d371bc4e19a480fecbdcda502d8b7349
Gol-D-monk <114142277+Gol-D-monk@users.noreply.github.com>
2024-04-14 14:42:49 +0530

    Add files via upload

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key = '88ea4c4f34c00d051cac396c963717cd'
+
+const requests = {
+    requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=horror&include_adult=false&language=en-US&page=1`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+
+};
+
+export default requests
